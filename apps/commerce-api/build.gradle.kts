c11fff26175691290ea80fd11927560930adefbe
uitae <40047335+lutae2000@users.noreply.github.com>
2025-09-05 01:22:25 +0900

    카프카 설정

--- apps/commerce-api/build.gradle.kts
+++ apps/commerce-api/build.gradle.kts
@@ -5,10 +5,12 @@ dependencies {
     implementation(project(":supports:logging"))
     implementation(project(":supports:monitoring"))
     implementation(project(":modules:redis"))
+    implementation(project(":modules:kafka"))
 
     // web
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot:spring-boot-starter-actuator")
+    implementation("org.springframework.boot:spring-boot-starter-aop")
     implementation ("org.springframework.cloud:spring-cloud-starter-openfeign")
     implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:${project.properties["springDocOpenApiVersion"]}")
     
@@ -17,10 +19,12 @@ dependencies {
     implementation("io.github.resilience4j:resilience4j-circuitbreaker")
     implementation("io.github.resilience4j:resilience4j-annotations")
 
-    // querydsl
-    implementation("com.querydsl:querydsl-jpa::jakarta")
+    // querydsl is provided via :modules:jpa
 
     // test-fixtures
     testImplementation(testFixtures(project(":modules:jpa")))
     testImplementation(testFixtures(project(":modules:redis")))
+    testImplementation(testFixtures(project(":modules:kafka")))
+    testImplementation("org.springframework.kafka:spring-kafka-test")
+    testImplementation("org.springframework.kafka:spring-kafka")
 }
